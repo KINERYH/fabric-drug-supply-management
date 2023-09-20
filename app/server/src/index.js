043c4def563e565df6f237504cdbf19a810a4e34
Bottrap <74072843+Bottrap@users.noreply.github.com>
2023-09-21 01:26:03 +0200

    divisione logica dell'app di test in più funzioni utilizzabili dal server

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const drugsRouter = require("./routes/drugs.routes")
+const drugsRouter = require("./routes/drugs.routes");
+const auth = require("./utils/blockchain/authentication");
+
+
+const { ccp, caClient, wallet } = auth.setupBlockchainApplicationConfig();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
-  });
+});
 
 app.use("/api/drugs", drugsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = {
+  ccp,
+  caClient,
+  wallet
+};
