a1f597555a509d79282828c03ee7be0d3742ce7a
francescodiana99 <francescodiana99@gmail.com>
2023-10-04 18:30:15 +0200

    updated DoctorContract

--- app/server/src/routes/test.routes.js
+++ app/server/src/routes/test.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const ledger = require("../utils/blockchain/connection");
 const { chaincodeName, channelName } = require("../config/blockchain");
 
@@ -33,9 +34,62 @@ router.get("/pharmacy/:pharmacyID", async (req, res) => {
 	const { ccp, wallet } = require("../index");
 	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'PharmacyContract');
 	const result = await contract.evaluateTransaction('GetPharmacyStorage', req.params.pharmacyID);
+	console.log("Result: \n",result);
 	const storage = JSON.parse(result.toString());
 	console.log('*** List:', JSON.stringify(storage, null, 2));
 	res.json({ status: "OK", data: storage });
 });
+
+// Test route per il DoctorContract
+router.get("/doctors/prescription/:doctorID", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	const result = await contract.evaluateTransaction('GetAllPrescriptions', req.params.doctorID);
+	const doctor = JSON.parse(result.toString());
+	res.json({ status: "OK", data: doctor });
+});
+
+router.get("/doctors/name/:doctorID", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	const result = await contract.evaluateTransaction('GetName', req.params.doctorID);
+	const name = result.toString();
+	res.json({ status: "OK", data: name });
+});
+
+router.get("/doctors/surname/:doctorID", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	const result = await contract.evaluateTransaction('GetSurname', req.params.doctorID);
+	const surname = result.toString();
+	res.json({ status: "OK", data: surname });
+});
+
+router.get("/doctors/hospital/:doctorID", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	const result = await contract.evaluateTransaction('GetHospital', req.params.doctorID);
+	const hospital = result.toString();
+	res.json({ status: "OK", data: hospital });
+});
+
+router.get("/doctors/specialization/:doctorID", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	const result = await contract.evaluateTransaction('GetSpecialization', req.params.doctorID);
+	const specialization = result.toString();
+	res.json({ status: "OK", data: specialization });
+});
+
+router.post("/doctors/prescriptions", async (req, res) => {
+	const { ccp, wallet } = require("../index");
+	console.log(typeof(req.body.PatientID), typeof(req.body.DoctorID), typeof(req.body.Drugs), typeof(req.body.Description));
+	const { contract } = await ledger.connect(ccp, wallet, 'admin', channelName, chaincodeName, 'DoctorContract');
+	//async CreatePrescription(ctx, docID, patientID, drugs, description) {
+	const result = await contract.submitTransaction('CreatePrescription', req.body.DoctorID, req.body.PatientID, req.body.Drugs, req.body.Description);
+	res.json({ status: "OK", data: result });
+});
+
+
 
 module.exports = router;
--- fabric-blockchain/asset-transfer-basic/chaincode-javascript/lib/doctorContract.js
+++ fabric-blockchain/asset-transfer-basic/chaincode-javascript/lib/doctorContract.js
@@ -3,24 +3,143 @@
 const stringify  = require('json-stringify-deterministic');
 const sortKeysRecursive  = require('sort-keys-recursive');
 const { Contract } = require('fabric-contract-api');
+const { v4: uuidv4 } = require('uuid');
+
 
 class DoctorContract extends Contract {
 
-  async GetAllPatients(ctx) {
-    const allPatients = await ctx.stub.getState('patients');
-    return allPatients.toString();
+
+  /**
+   * Retrieves all the prescriptions made by a specific doctor from the ledger.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} docID - The ID of the doctor.
+   * @returns {Promise<string>} - The list of all prescriptions as a string.
+   */
+  async GetAllPrescriptions(ctx, docID) {
+    const serializedPrescriptions = await ctx.stub.getState('prescriptions');
+    if (!serializedPrescriptions || serializedPrescriptions.length === 0) {
+      throw new Error(`There are no prescriptions in the ledger`);
+    }
+    const prescriptions = JSON.parse(serializedPrescriptions.toString());
+    const docPrescriptions = prescriptions.filter(prescription => prescription.DoctorID === docID);
+    return docPrescriptions;
   }
 
-  async GetPatient(ctx, cf) {
-    const patient = await ctx.stub.getState('patients');
-    
-    return patient.toString();
+
+  /**
+   * Get the name of a doctor with the given ID.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} docID - The ID of the doctor to retrieve the name for.
+   * @returns {string} - The name of the doctor with the given ID.
+   * @throws Will throw an error if there are no doctors in the ledger.
+   */
+  async GetName(ctx, docID) {
+    const serializedDoctors = await ctx.stub.getState('doctors');
+    if (!serializedDoctors || serializedDoctors.length === 0) {
+      throw new Error(`There are no doctors in the ledger`);
+    }
+    const doctors = JSON.parse(serializedDoctors.toString());
+    const doctor = doctors.find(doctor => doctor.ID === docID);
+    return doctor.Name;
   }
 
-  async GetPatientbyName(ctx, name, surrname) {
-    const patient = await ctx.stub.getState(name+surrname);
-    return patient.toString();
+  /**
+   * Get the surname of a doctor with the given ID.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} docID - The ID of the doctor to retrieve the name for.
+   * @returns {string} - The surname of the doctor with the given ID.
+   * @throws Will throw an error if there are no doctors in the ledger.
+   */
+  async GetSurname(ctx, docID) {
+    const serializedDoctors = await ctx.stub.getState('doctors');
+    if (!serializedDoctors || serializedDoctors.length === 0) {
+      throw new Error(`There are no doctors in the ledger`);
+    }
+    const doctors = JSON.parse(serializedDoctors.toString());
+    const doctor = doctors.find(doctor => doctor.ID === docID);
+    return doctor.Surname;
   }
+
+  /**
+   * Get the Hospital of a doctor with the given ID.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} docID - The ID of the doctor to retrieve the name for.
+   * @returns {string} - The Hospital of the doctor with the given ID.
+   * @throws Will throw an error if there are no doctors in the ledger.
+   */
+  async GetHospital(ctx, docID) {
+    const serializedDoctors = await ctx.stub.getState('doctors');
+    if (!serializedDoctors || serializedDoctors.length === 0) {
+      throw new Error(`There are no doctors in the ledger`);
+    }
+    const doctors = JSON.parse(serializedDoctors.toString());
+    const doctor = doctors.find(doctor => doctor.ID === docID);
+    return doctor.Hospital;
+  }
+
+  /**
+   * Get the specialization of a doctor with the given ID.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} docID - The ID of the doctor to retrieve the name for.
+   * @returns {string} - The specialization of the doctor with the given ID.
+   * @throws Will throw an error if there are no doctors in the ledger.
+   */
+  async GetSpecialization(ctx, docID) {
+    const serializedDoctors = await ctx.stub.getState('doctors');
+    if (!serializedDoctors || serializedDoctors.length === 0) {
+      throw new Error(`There are no doctors in the ledger`);
+    }
+    const doctors = JSON.parse(serializedDoctors.toString());
+    const doctor = doctors.find(doctor => doctor.ID === docID);
+    return doctor.Specialization;
+  }
+
+   /**
+   * Check if a prescription exists.
+   * @async
+   * @param {Context} ctx - The transaction context object.
+   * @param {string} prescriptionID - The ID of the prescription to check.
+   * @returns {boolean} - True or False.
+   * @throws Will throw an error if the prescription already exists.
+   */
+  async PresciptionExists(ctx, prescriptionID) {
+    const prescriptions = await ctx.stub.getState('prescriptions');
+    if (!prescriptions || prescriptions.length === 0) {
+      throw new Error(`No prescription in the ledger`);
+    }
+    const exist = prescriptions.find(prescription => prescription.ID === prescriptionID)
+    if (exist) {
+      throw new Error(`Prescription ${prescriptionID} already exists`);
+    }
+    return exist;
+  }
+
+  //GUARDA async InitLedger(ctx, initState) l'oggetto dovrebbe essere stringificato prima di essere passato,
+  //es. https://github.com/tulios/kafkajs/issues/1019
+  async CreatePrescription(ctx, docID, patientID, drugs, description) {
+    let prescriptionID = uuidv4();
+    const exists = await this.PresciptionExists(ctx, prescriptionID);
+    while (exists) {
+      prescriptionID = uuidv4();
+    }
+
+    const prescription = {
+      DoctorID: docID,
+      PatientID: patientID,
+      Drugs: drugs,
+      Description: description,
+      Status: "pending"
+    };
+    await ctx.stub.putState('prescriptions', Buffer.from(sortKeysRecursive(prescription)));
+    return JSON.stringify(prescription);
+  }
+
+
 
 }
 
